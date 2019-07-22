308cb6ed1883c5080ebab6e3aefc6c37563903f3
dbs-montoya <danmontoya001@gmail.com>
2019-07-22 11:14:18 -0400

    first Commit, created a react app, cleaned up app, brought in: Routing, Folder Structure, GSAP, and Sass

---
+++ src/Pages/1.Landing/Landing.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Landing.scss';
+
+export default function Landing() {
+    return (
+        <div className='Landing'>
+            <h1>Hello From Landing</h1>
+        </div>
+    )
+}
