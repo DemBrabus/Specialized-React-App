0d597b8b447cca97a718aa53974aab2043a8fee7
dbs-montoya <danmontoya001@gmail.com>
2019-07-26 23:04:04 -0400

    working on About page, not liking the look, needs some work :/

--- src/Pages/Inside-Specialized/InsideSpecialized.js
+++ src/Pages/Inside-Specialized/InsideSpecialized.js
@@ -12,29 +12,34 @@ export default function InsideSpecialized() {
                     <p className='Inside-Section1-Text'>Specialized. Made for riders, by riders.</p>
                 </div>
 
+                        <div className='Inside-Section1-Mobile'>
+                                <p className='Inside-Section1-Title-Mobile'>1974</p>
+                        <p className='Inside-Section1-Text-Mobile'>We are riders—that fact has guided our every decision since 1974. When quality tires weren't around, we strove to make the best. When people wanted to ride cruisers in the dirt, we made the first production mountain bike. When roadies wanted to go faster, we doubled-down on carbon and built our own wind tunnel. And when we saw kids struggling to focus in school, we started the Specialized Foundation to help them through cycling.</p>
+                        </div>
+
                 <div className='Inside-Section2'>
 
                 </div>
 
-                <div className='Inside-Section4'>
+                <div className='Inside-Section3'>
                     <p className='Inside-Section-Text'>SINCE THERE’VE BEEN MOUNTAIN BIKES, THERE’VE BEEN STUMPJUMPERS</p>
                 </div>
 
-                <div className='Inside-Section5'>
+                <div className='Inside-Section4'>
                     
                 </div>
 
-                <div className='Inside-Section6'>
+                <div className='Inside-Section5'>
                     <p className='Inside-Section-Text'>WE BELIEVE CYCLING CHANGES LIVES</p>
                 </div>
 
-                <div className='Inside-Section7'>
+                <div className='Inside-Section6'>
                     <iframe width="100%" height="100%" src="https://www.youtube.com/embed/5emg5HORbaY" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                 </div>
 
-                 <div className='Inside-Section8'>
-                    <p className='Inside-Section8-Title'>WHY WORK HERE</p>
-                    <p className='Inside-Section8-Text'>If there’s one thing that unites the people who work here, it’s this: We believe that bicycles have the power to change lives. That’s why we’re always looking for passionate people from all disciplines who share in this belief and who are looking to play an active role in inspiring and getting more people on bikes. Are you ready to make a difference?</p>
+                 <div className='Inside-Section7'>
+                    <p className='Inside-Section7-Title'>WHY WORK HERE</p>
+                    <p className='Inside-Section7-Text'>If there’s one thing that unites the people who work here, it’s this: We believe that bicycles have the power to change lives. That’s why we’re always looking for passionate people from all disciplines who share in this belief and who are looking to play an active role in inspiring and getting more people on bikes. Are you ready to make a difference?</p>
                     <div className='Inside-Section8-Button'>SEARCH JOB OPENINGS</div>
                 </div>
 
